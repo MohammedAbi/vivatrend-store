87f11461f3bac64ba725d6d0be0fe9a7f29377c7
Mohammed Abdul Abi <mohammedabdulabi@gmail.com>
2025-05-10 22:46:24 +0200

    Updated notfound page style

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,15 +1,20 @@
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-full text-center p-10 bg-gray-100">
+    <div className="flex flex-col items-center justify-center h-full text-center pt-[180px] pb-40 bg-gray-100">
       <h1 className="h2 text-red-600">404</h1>
       <p className="h3 text-gray-700">Page Not Found</p>
       <p className="text-lg mt-4 text-gray-500">
         The page you're looking for doesn't exist.
       </p>
       <div className="mt-6">
-        <a href="/" className="btn btn-primary text-lg">
+        <Link
+          to="/"
+          className="btn btn-lg btn-primary py-3 px-6 text-sm sm:text-base"
+        >
           Go Back Home
-        </a>
+        </Link>
       </div>
     </div>
   );
